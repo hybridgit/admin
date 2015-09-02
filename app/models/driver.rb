c789057cae72aead1ea2700424bff148dab3ef10
Ameer Haider <ameer@novatoresols.com>
2015-09-02 10:48:50 +0500

    fixed driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,18 @@
 class Driver < ActiveRecord::Base
   attr_accessible :profile_id, :name, :cell_no, :location, :location_lat, :location_long
+  has_many :cab_requests
+	has_many :driver_lists
+    acts_as_mappable :default_units => :kms,
+                   :default_formula => :sphere,
+                   :lat_column_name => :location_lat,
+                   :lng_column_name => :location_long
+
+  def self.is_not_driver(cell_no)
+    @driver=Driver.where(:cell_no=>cell_no).last
+    if @driver.present?
+      return false
+    else
+      return true
+    end
+  end
 end
