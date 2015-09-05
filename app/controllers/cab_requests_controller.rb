1eeb3c4e84b787aa8e903bbcc4f03248ff4190a1
Ameer Haider <ameer@novatoresols.com>
2015-09-06 00:24:19 +0500

    +251 removed

--- app/controllers/cab_requests_controller.rb
+++ app/controllers/cab_requests_controller.rb
@@ -188,7 +188,7 @@ class CabRequestsController < ApplicationController
           @cab_request  = CabRequest.where(:deleted => false, :current_driver_id => @driver.id, :status => false).last
           if @cab_request.present?
             #Send sms to driver
-            @message = "Here we go... Your customer lives near "+@cab_request.location.to_s+". You must call him/her in 2 mins. Customer phone number: "+@cab_request.customer_cell_no.gsub!("+251", "0"))
+            @message = "Here we go... Your customer lives near "+@cab_request.location.to_s+". You must call him/her in 2 mins. Customer phone number: "+@cab_request.customer_cell_no.gsub!("+251", "0")
             send_message(@driver.cell_no, @message, @short_code)
             #Send sms to customer
             @message = SUCCESS_MESSAGE_ON_ARRANGEMENT
